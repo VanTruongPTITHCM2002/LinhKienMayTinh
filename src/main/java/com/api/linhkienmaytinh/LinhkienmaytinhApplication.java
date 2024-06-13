9008409281bc3244ad64d0f68882d7515b169338
VanTruongPTITHCM2002 <truongboss1234yahoo@gmail.com>
2024-06-13 17:30:59 +0700

    Xay dung web ban linh kien may tinh

---
+++ src/main/java/com/api/linhkienmaytinh/LinhkienmaytinhApplication.java
@@ -0,0 +1,13 @@
+package com.api.linhkienmaytinh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinhkienmaytinhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinhkienmaytinhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/linhkienmaytinh/LinhkienmaytinhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.linhkienmaytinh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinhkienmaytinhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
